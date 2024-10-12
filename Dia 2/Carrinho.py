bf5419df4e0a51c66036cf72926e959228900806
Samuel-SaaS <samusola23@gmail.com>
2024-10-12 13:47:03 -0300

    Delete Dia 2 directory

--- Dia 2/Carrinho.py
+++
@@ -1,6 +0,0 @@
-item = input('Oque você vai comprar? é ')
-preço = float(input('Qual é o preço do item? é '))
-qtd = int(input('Quantos desse item você vai comprar? '))
-t = preço*qtd
-
-print(f'O preço total do seu item deu R${t}.')
--- Dia 2/mad libs.py
+++
@@ -1,8 +0,0 @@
-print('Bem vindo ao Mad Libs.')
-cenário = input('Digite um cenário: ')
-pessoa = input('Digite o nome de alguém:')
-idade = int(input('Digite a suposta idade dessa pessoa: '))
-estado = input('Digite como essa pessoa está: ')
-desejo = input('Digite o que essa pessoa quer: ')
-
-print(f'Hoje, eu vi {pessoa} no(a) {cenário}, aparentemente ele tinha {idade}, e, por algum motivo, essa pessoa estava bem {estado} e ela me disse que queria {desejo}')
