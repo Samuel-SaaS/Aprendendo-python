776c978b894df6ccb361978f68af83558c2ffbba
Samuel-SaaS <samusola23@gmail.com>
2024-10-12 13:47:12 -0300

    Add files via upload

---
+++ Dia 2/Ret_Area_calc.py
@@ -0,0 +1,4 @@
+h = int(input('Digite quanto é a altura desse retângulo em cm: '))
+b = int(input('Digite quanto é a base desse retângulo em cm: '))
+a = b*h
+print(f'A área do seu retângulo é {a}cm^2')
