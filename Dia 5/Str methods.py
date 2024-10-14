e5e1553d407763945eeeea19535f546abcbf5060
Samuel-SaaS <samusola23@gmail.com>
2024-10-14 18:16:02 -0300

    Add files via upload

---
+++ Dia 5/Str methods.py
@@ -0,0 +1,16 @@
+# len() → conta quantos itens tem no que tá no parentes, se for str, contará quantas letras
+# X.find(' ') → contará quantos itens tem até chegar no item do parentes, começando do 0
+# X.rlen(' ') → Contará quantos itens tem até chegar no último item do parenteses, começando do 0
+# X.Capitalize() → capitalizará o texto
+# X.Upper() → deixará todos as letras maiúsculos
+# ..., mesmo esquema com X.isalpha, etc
+
+nome = input('Digite seu nome: ')
+
+# a = len(nome)
+# a = nome.find(' ')
+# a = nome.rfind(' ')
+# a = nome.subs()
+a = nome.alpha('')
+
+print(a)
